0a9acae58c44c7539467a3e0c85cb0081b14f847
Estevão Lucas <3stevaolucas@gmail.com>
2022-04-29 19:14:49 -0300

    Feito o metodo de geração de slug automatico e modificação de nomes no admin

--- produto/models.py
+++ produto/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+from django.utils.text import slugify
 from PIL import Image
 import os
 
@@ -13,10 +14,19 @@ class Produto(models.Model):
     descricao_longa = models.TextField()
     imagem = models.ImageField(upload_to='produto_imagens/%Y/%m/',
                               blank=True, null=True)
-    slug = models.SlugField(unique=True)
-    preco_marketing = models.FloatField()
-    preco_marketing_promocional = models.FloatField(default=0)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField(verbose_name='Preço')
+    preco_marketing_promocional = models.FloatField(default=0, verbose_name='Preço Promo')
     tipo = models.CharField(default='V', choices=TIPO, max_length=1)
+
+    def get_preco_formatado(self):
+        return f'{self.preco_marketing:.2f}'.replace('.', ',')
+
+    def get_preco_promo_formatado(self):
+        return f'{self.preco_marketing_promocional:.2f}'.replace('.', ',')
+
+    get_preco_formatado.short_description = 'Preço'
+    get_preco_promo_formatado.short_description = 'Preço Promocional'
 
     @staticmethod
     def resize_image(img, nova_largura=800):
@@ -41,6 +51,10 @@ class Produto(models.Model):
         )
 
     def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.nome)}'
+            self.slug = slug
+
         super().save(*args, **kwargs)
 
         max_image_size = 800
