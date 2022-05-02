765bb3ae5bad523531687475219a960aa17a3d01
Estevão Lucas <3stevaolucas@gmail.com>
2022-05-02 17:05:44 -0300

    Feito view de Listar Produtos

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+
+from .models import Produto
 
 # Create your views here.
+class ListaProdutos(ListView):
+    model = Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 10
+
+class DetalheProduto(View):
+    pass
+
+class AddCarrinho(View):
+    pass
+
+class RmCarrinho(View):
+    pass
+
+class Finalizar(View):
+    pass
+
+class Carrinho(View):
+    pass
