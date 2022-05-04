b77111949847ef75160e8f2cb9edb3ddb201a1d8
Estevão Lucas <3stevaolucas@gmail.com>
2022-05-04 18:05:17 -0300

    modificado o name da url de add ao carrinho

--- produto/urls.py
+++ produto/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.ListaProdutos.as_view(), name="lista"),
     path('<slug>', views.DetalheProduto.as_view(), name="detalhe"),
     path('addcarrinho', views.AddCarrinho.as_view(),
-         name="adicionar ao carrinho"),
+         name="adicionaraocarrinho"),
     path('rmcarrinho', views.RmCarrinho.as_view(),
          name="removerdocarrinho"),
     path('carrinho', views.Carrinho.as_view(), name="carrinho"),
